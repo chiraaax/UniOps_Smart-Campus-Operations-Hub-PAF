7b56401642b6404b9a51781617613fa3beb0d4bc
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2026-04-06 00:20:00 +0530

    Add initial project structure with Maven configuration, model classes, and updated .gitignore

---
+++ demo/src/main/java/com/uniops/demo/model/Facility.java
@@ -0,0 +1,20 @@
+package com.uniops.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "facilities")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Facility {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
