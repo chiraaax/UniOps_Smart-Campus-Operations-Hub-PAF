c79b223a929a33e34e4817d666d271a437f3650b
janakashan <janakashan190@gmail.com>
2026-04-18 13:13:19 +0530

    Refactor unused controllers, models, repositories, and services

--- demo/src/main/java/com/uniops/demo/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.uniops.demo.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String email;
-    private String password;
-}
--- demo/src/main/java/com/uniops/demo/enums/ResourceStatus.java
+++
@@ -1,7 +0,0 @@
-package com.uniops.demo.enums;
-
-public enum ResourceStatus {
-    ACTIVE,
-    OUT_OF_SERVICE,
-    UNDER_MAINTENANCE
-}
