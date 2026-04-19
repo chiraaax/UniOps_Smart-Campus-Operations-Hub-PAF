7c0f654c6f8cc6f17385a52ab2c7193a6e3d2488
Tharu012 <tharushibandara789@gmail.com>
2026-04-19 12:20:29 +0530

    Refactor: Remove Booking, Facility, Incident, Notification, and User modules

--- backend/src/main/java/com/sliit/smartcampus/common/enums/FacilityStatus.java
+++
@@ -1,6 +0,0 @@
-package com.sliit.smartcampus.common.enums;
-
-public enum FacilityStatus {
-    ACTIVE,
-    OUT_OF_SERVICE
-}
--- backend/src/main/java/com/sliit/smartcampus/common/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.sliit.smartcampus.common.enums;
-
-public enum Role {
-    USER,
-    ADMIN,
-    TECHNICIAN
-}
